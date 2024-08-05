1cea88198e2b7fa2657dbde54d0046f59594e810
sangam shinde <sangampshinde@gmail.com>
2024-08-05 22:00:12 +0530

    geting test resiter form data using axios

--- backend/index.js
+++ backend/index.js
@@ -2,10 +2,13 @@ const express = require('express');
 // require("dotenv").config();
 const dotenv = require('dotenv');
 const mongoose = require('mongoose');
+const jwt = require("jsonwebtoken");
+const User = require("./models/User.js");
 
 dotenv.config();
 
 mongoose.connect(process.env.MONGO_URL);
+const jwtSecret = process.env.JWT_SECRET;
 
 
 const app = express();
@@ -16,7 +19,15 @@ app.get('/test', (req, res) => {
 
 
 // register
-app.post('/register', (req, res) => {})
+app.post('/register', async (req, res) => {
+    const {username,password} = req.body;
+    const createdUser = await User.create({username,password});
+
+    jwt.sign({userId:createdUser._id},jwtSecret,(err,token)=>{
+        if(err) throw err;
+        res.cookie("token",token).status(201).json("ok");
+    })
+})
 
 app.listen(4000);
 
--- chat app/src/App.jsx
+++ chat app/src/App.jsx
@@ -1,6 +1,12 @@
+import axios from "axios"
 import Register from "./register"
 
 function App() {
+
+  axios.defaults.baseURL ="http://localhost:4000";
+  axios.defaults.withCredentials = true; // to send cookies with the requests
+  
+
 
   return (
     <Register/>
