d2e33cbf6cac40b6831bc9c72757216211fa12f2
sangam shinde <sangampshinde@gmail.com>
2024-08-13 21:09:00 +0530

    message model and conversation model creating

---
+++ backend/models/message.model.js
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+
+const messageSchema = mongoose.Schema({
+    senderId:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    receiverId:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    message:{
+        type: String,
+        required: true
+    }
+// createdAt and updatedAt
+},{timestamp:true})
+
+
+const Message = mongoose.model('Message', messageSchema);
+
+export default Message;
